c464eed22f3e3d05ea8b33ecf927a57d135eaa4e
RameesParambil <rameesparambil2001@gmail.com>
2025-08-05 00:22:20 +0530

    update requirements.txt file

--- myproject5/settings.py
+++ myproject5/settings.py
@@ -76,12 +76,23 @@ WSGI_APPLICATION = 'myproject5.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/'db.sqlite3',
+if os.getenv("VERCEL", "").lower() == "1":
+    # Running on Vercel → use DATABASE_URL
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.getenv("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True
+        )
     }
-}
+else:
+    # Local development: default sqlite3
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
